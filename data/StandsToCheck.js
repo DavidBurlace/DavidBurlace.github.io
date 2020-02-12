aba7e9f63a1baeead9d33830877ab646d0c1bed1
DavidBurlace <davidburlace@gmail.com>
2020-02-13 10:57:23 +1300

    Update StandsToCheck.js

--- data/StandsToCheck.js
+++ data/StandsToCheck.js
@@ -3,7 +3,6 @@
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
 { "type": "Feature", "properties": { "Asset_Desc": "Steel pipe bike stands near Davies St entrance, next to scooter rack.", "Model_of_C": "Not Applicable", "Comments": null}, "geometry": { "type": "MultiPoint", "coordinates": [ [ 174.828843599624633, -41.164208972758153 ], [ 174.828834699723075, -41.164206492970202 ] ] } },
-{ "type": "Feature", "properties": { "Asset_Desc": "Under cliffs south of boardwalk from Steeple Lane", "Model_of_C": "Not Applicable", "Comments": null}, "geometry": { "type": "MultiPoint", "coordinates": [ [ 174.838598130246936, -41.326162842362116 ] ] } },
 { "type": "Feature", "properties": { "Asset_Desc": "FB51 - Bike Stand MCMILLAN CT", "Model_of_C": "FB51 - Bike Stand", "Comments": null, }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 174.822748110581387, -41.222816547382308 ] ] } },
 { "type": "Feature", "properties": { "Asset_Desc": "FB51 - Bike Stand MCMILLAN CT", "Model_of_C": "FB51 - Bike Stand", "Comments": null, }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 174.822755419064521, -41.222822483652642 ] ] } },
 { "type": "Feature", "properties": { "Asset_Desc": "FB51 - Bike Stand MCMILLAN CT", "Model_of_C": "FB51 - Bike Stand", "Comments": null, }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 174.82294820520238, -41.222750195549331 ] ] } },
